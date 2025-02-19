d4bdbc05c6d7ce06a17cf9bf423130b2740dfdeb
Nipush <meirelesramos@gmail.com>
2025-02-19 02:35:12 -0300

    with FPS part

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
+    dt = 0 # Delta time
     pygame.init()  # Initialize pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Create the screen
+    pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,8 @@ def main():
 
         screen.fill((0, 0, 0))  # Fill the screen with black
         pygame.display.flip()  # Update the screen
+        pygame.time.Clock().tick(60)  # Set the frame rate to 60
+        dt = pygame.time.Clock().tick(60) / 1000  # Get the delta time
 
 
 
